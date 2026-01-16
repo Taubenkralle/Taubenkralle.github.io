bb1f327c56cf46dc51acb8d7057b63e24ae1a416
Taubenkralle <maximil7on@mail.de>
2026-01-16 13:51:29 +0100

    Add rain toggle on 8 with home fixed

--- view.js
+++ view.js
@@ -1,6 +1,7 @@
 (() => {
   const KEY = "wiki.view.wide";
   const UI_KEY = "wiki.view.uiHidden";
+  const RAIN_KEY = "wiki.view.rainHidden";
 
   const toggleBtn = document.querySelector(".view-toggle");
 
@@ -26,12 +27,24 @@
     }
   }
 
+  function setRainHidden(on){
+    document.body.classList.toggle("rain-hidden", on);
+    if (on){
+      localStorage.setItem(RAIN_KEY, "1");
+    }else{
+      localStorage.removeItem(RAIN_KEY);
+    }
+  }
+
   function init(){
     if (localStorage.getItem(KEY) === "1"){
       setWide(true);
     }
     if (localStorage.getItem(UI_KEY) === "1"){
       setUiHidden(true);
+    }
+    if (!document.body.dataset.rainFixed && localStorage.getItem(RAIN_KEY) === "1"){
+      setRainHidden(true);
     }
 
     if (toggleBtn){
@@ -55,6 +68,10 @@
       if (e.key === "7"){
         e.preventDefault();
         setUiHidden(!document.body.classList.contains("ui-hidden"));
+      }
+      if (e.key === "8" && !document.body.dataset.rainFixed){
+        e.preventDefault();
+        setRainHidden(!document.body.classList.contains("rain-hidden"));
       }
     });
   }
