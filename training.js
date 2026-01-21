f34ce446b6de225702506b49b6adb49d120576a1
Taubenkralle <maximil7on@mail.de>
2026-01-21 09:51:42 +0100

    Increase difficulty further

--- training.js
+++ training.js
@@ -39,10 +39,10 @@
   const MAX_ENEMIES = 120;
   const LOGIC_DT = 1 / 30;
   const MAX_SHOTS = 240;
-  const DIFF_HP_MULT = 1.35;
-  const DIFF_SPEED_MULT = 1.18;
-  const DIFF_LATE_HP_MULT = 1.25;
-  const DIFF_LATE_SPEED_MULT = 1.12;
+  const DIFF_HP_MULT = 1.45;
+  const DIFF_SPEED_MULT = 1.22;
+  const DIFF_LATE_HP_MULT = 1.32;
+  const DIFF_LATE_SPEED_MULT = 1.16;
 
   const sfx = {
     ctx: null,
